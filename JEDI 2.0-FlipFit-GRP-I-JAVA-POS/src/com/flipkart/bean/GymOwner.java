169d4459f9ec1714c97c7710f8ecf3c05eac9424
srishtayy <srishtidhir652@gmail.com>
2024-07-16 17:10:36 +0530

    Added GymOwner Bean

---
+++ JEDI 2.0-FlipFit-GRP-I-JAVA-POS/src/com/flipkart/bean/GymOwner.java
@@ -0,0 +1,52 @@
+package com.flipkart.bean;
+import java.util.List;
+
+public class GymOwner extends User {
+
+
+    public String getPanNumber() {
+        return panNumber;
+    }
+
+    public void setPanNumber(String panNumber) {
+        this.panNumber = panNumber;
+    }
+
+    public boolean isApproved() {
+        return isApproved;
+    }
+
+    public void setApproved(boolean approved) {
+        isApproved = approved;
+    }
+
+    public List<String> getGymCentreLists() {
+        return gymCentreLists;
+    }
+
+    public void setGymCentreLists(List<String> gymCentreLists) {
+        this.gymCentreLists = gymCentreLists;
+    }
+
+    public String getCardDetails() {
+        return cardDetails;
+    }
+
+    public void setCardDetails(String cardDetails) {
+        this.cardDetails = cardDetails;
+    }
+
+
+    public GymOwner(String userId, String userName, String email, String password,String panNumber, boolean isApproved, List<String> gymCentreLists, String cardDetails) {
+        super(userId, userName, email, password);
+        this.panNumber = panNumber;
+        this.isApproved = isApproved;
+        this.gymCentreLists = gymCentreLists;
+        this.cardDetails = cardDetails;
+    }
+
+    private String panNumber;
+    private boolean isApproved;
+    private List<String> gymCentreLists;
+    private String cardDetails;
+}
