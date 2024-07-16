ebee668c046592e8eeac2cf39b5f3d0416ac8af6
srishtayy <srishtidhir652@gmail.com>
2024-07-16 17:20:07 +0530

    Added GymOwnerInterface

---
+++ JEDI 2.0-FlipFit-GRP-I-JAVA-POS/src/com/flipkart/business/GymOwnerInterface.java
@@ -0,0 +1,22 @@
+package com.flipkart.business;
+
+import com.flipkart.bean.GymCentre;
+import com.flipkart.bean.Slot;
+
+import java.sql.Date;
+import java.util.List;
+
+public interface GymOwnerInterface {
+    public void login();
+
+    public void register();
+
+    public List<GymCentre> getAllCentresByOwnerId(String gymOwnerId);
+
+    public List<Slot> getSlotsByCentre(String centreID, Date date);
+
+    public void addCenter(GymCentre gymCentre);
+
+    public void requestGymCentreApproval(String gymCentreId);
+
+}
