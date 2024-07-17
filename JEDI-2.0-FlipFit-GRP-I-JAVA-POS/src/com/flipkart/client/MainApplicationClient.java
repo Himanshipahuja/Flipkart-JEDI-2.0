87176bb27b6179578963c170b0292438069257c6
gaurang2002 <gaurangthakur7890@gmail.com>
2024-07-17 12:00:31 +0530

    Added Main Application

--- JEDI-2.0-FlipFit-GRP-I-JAVA-POS/src/com/flipkart/client/MainApplicationClient.java
+++ JEDI-2.0-FlipFit-GRP-I-JAVA-POS/src/com/flipkart/client/MainApplicationClient.java
@@ -1,12 +1,129 @@
 package com.flipkart.client;
+import com.flipkart.bean.Role;
 
-import com.flipkart.bean.Customer;
+
+import java.util.Scanner;
+
+import static com.flipkart.constants.Constants.INVALID_CHOICE_ERROR;
+import static com.flipkart.constants.Constants.WELCOME_MESSAGE;
 
 public class MainApplicationClient {
 
+    public static int userId = 0;
+    public static Scanner scanner = new Scanner(System.in);
+    private static AdminClient adminClient = new AdminClient();
+    private static CustomerClient customerClient = new CustomerClient();
+    private static GymOwnerClient gymOwnerClient = new GymOwnerClient();
+
+
+    private static void mainPage(){
+        System.out.println("1. Login\n2. Registration of the GYM Customer\n3. Registration of the GYM Owner\n4. Change Password\n5. Exit");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+                login();
+                break;
+            case 2:
+                registration("CUSTOMER");
+                break;
+            case 3:
+                registration("GYMOWNER");
+                return;
+            case 4:
+                change_password();
+                return;
+            default:
+                System.out.println(INVALID_CHOICE_ERROR);
+                break;
+        }
+        mainPage();
+    }
+
+    private static void login(){
+        try {
+            System.out.println("Enter your Role");
+            Role role = Role.valueOf(scanner.next().toUpperCase());
+
+            System.out.println("Enter your UserName");
+            String userName = scanner.next();
+
+            System.out.println("Enter your Passkey");
+            String password = scanner.next();
+
+            switch (role){
+                case ADMIN:
+                    adminClient.adminLogin(userName,password);
+                    break;
+                case GYMOWNER:
+                    gymOwnerClient.gymOwnerLogin(userName,password);
+                    break;
+                case CUSTOMER:
+                    customerClient.customerLogin(userName,password);
+                    break;
+                default:
+                    System.out.println(INVALID_CHOICE_ERROR);
+                    break;
+            }
+        }catch (IllegalArgumentException e){
+            System.out.println(INVALID_CHOICE_ERROR);
+        }
+    }
+
+    private static void registration(String role){
+        try {
+            switch (role){
+                case ADMIN:
+                    System.out.println("Admin is already registered");
+                    mainPage();
+                    break;
+                case CUSTOMER:
+                    customerClient.register();
+                    break;
+                case GYMOWNER:
+                    gymOwnerClient.register();
+                    break;
+                default:
+                    System.out.println(INVALID_CHOICE_ERROR);
+                    break;
+            }
+        }catch (IllegalArgumentException e){
+            System.out.println(INVALID_CHOICE_ERROR);
+        }
+    }
+    private static void change_password(){
+        try {
+            System.out.println("Enter your Role");
+            Role role = Role.valueOf(scanner.next().toUpperCase());
+
+            System.out.println("Enter your UserName");
+            String userName = scanner.next();
+
+            System.out.println("Enter your Old Passkey");
+            String old_password = scanner.next();
+            System.out.println("Enter your New Passkey");
+            String new_password = scanner.next();
+
+            switch (role){
+                case ADMIN:
+                    adminClient.adminChangePassword(userName,old_password,new_password);
+                    break;
+                case GYMOWNER:
+                    gymOwnerClient.gymChangePassword(userName,old_password,new_password);
+                    break;
+                case CUSTOMER:
+                    customerClient.customerChangePassword(userName,old_password,new_password);
+                    break;
+                default:
+                    System.out.println(INVALID_CHOICE_ERROR);
+                    break;
+            }
+        }catch (IllegalArgumentException e){
+            System.out.println(INVALID_CHOICE_ERROR);
+        }
+    }
+
     public static void main(String[] args) {
-        Customer customer = new Customer();
-        customer.setUserName("Abc");
-        System.out.println(customer.getUserName());
+        System.out.println(WELCOME_MESSAGE);
+        mainPage();
     }
 }
--- JEDI-2.0-FlipFit-GRP-I-JAVA-POS/src/com/flipkart/constants/Constants.java
+++ JEDI-2.0-FlipFit-GRP-I-JAVA-POS/src/com/flipkart/constants/Constants.java
@@ -1,4 +1,7 @@
 package com.flipkart.constants;
 
 public class Constants {
+    public static String INVALID_CHOICE_ERROR = "Please enter valid choice\n";
+    public static String WELCOME_MESSAGE = "WELCOME TO THE GMS APPLICATION\n";
+
 }
