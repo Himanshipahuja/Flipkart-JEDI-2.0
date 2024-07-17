5bf0278a9b40eb76736b51dd7dcc3d7df3e21422
ipriyanshi1708 <priyanshiagarwal1708@gmail.com>
2024-07-17 11:42:36 +0530

    Renamed MainApplicationClient

--- JEDI-2.0-FlipFit-GRP-I-JAVA-POS/src/com/flipkart/client/MainApplicationClient.java
+++ JEDI-2.0-FlipFit-GRP-I-JAVA-POS/src/com/flipkart/client/MainApplicationClient.java
@@ -2,7 +2,7 @@ package com.flipkart.client;
 
 import com.flipkart.bean.Customer;
 
-public class FlipFitClient {
+public class MainApplicationClient {
 
     public static void main(String[] args) {
         Customer customer = new Customer();
