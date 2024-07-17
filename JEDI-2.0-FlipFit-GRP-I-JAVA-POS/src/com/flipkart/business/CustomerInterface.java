df373bfb717f030b08eb30c3d4f856388b343b07
ridhi794 <ridhi794@gmail.com>
2024-07-17 10:05:31 +0530

    CustomerInterface Added

---
+++ JEDI-2.0-FlipFit-GRP-I-JAVA-POS/src/com/flipkart/business/CustomerInterface.java
@@ -0,0 +1,19 @@
+package com.flipkart.business;
+
+import com.flipkart.bean.Booking;
+import com.flipkart.bean.Customer;
+import com.flipkart.bean.GymCentre;
+
+import java.sql.Date;
+import java.util.List;
+
+public interface CustomerInterface {
+    void CustomerLogin();
+    List<GymCentre> getAllGymCenterDetailsByCity(String city);
+    List<Booking> getCustomerBookings(String customerId);
+    boolean bookSlot(String userID,Date date, String slotId,String centreId);
+    void cancelBooking(String bookingID);
+    void registerCustomer(String userName, String password, String email, String phoneNumber, String cardNumber);
+
+
+}
