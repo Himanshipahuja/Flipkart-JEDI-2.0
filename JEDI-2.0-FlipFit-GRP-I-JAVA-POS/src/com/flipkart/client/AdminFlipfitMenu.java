28657be86a680d6ec7158f514675caf9fee318ba
ridhi794 <ridhi794@gmail.com>
2024-07-17 15:41:06 +0530

    Admin menu added

--- JEDI-2.0-FlipFit-GRP-I-JAVA-POS/src/com/flipkart/client/AdminFlipfitMenu.java
+++ JEDI-2.0-FlipFit-GRP-I-JAVA-POS/src/com/flipkart/client/AdminFlipfitMenu.java
@@ -3,11 +3,16 @@ package com.flipkart.client;
 import com.flipkart.bean.Admin;
 import com.flipkart.exceptions.LoginFailedException;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Objects;
+import java.util.Scanner;
 
 
 public class AdminFlipfitMenu {
-     Admin admin = new Admin();
+    Admin admin = new Admin();
+
+    public static Scanner scanner = new Scanner(System.in);
 
     public boolean isUserValid(String userName, String password) {
         if (userName.equals(admin.getUserName()) && password.equals(admin.getPassword())) {
@@ -15,29 +20,50 @@ public class AdminFlipfitMenu {
         }
         return false;
     }
+
     public boolean adminLogin(String userName, String password) {
         if (isUserValid(userName, password)) {
             System.out.println("Successfully logged in as admin");
+            adminClientMainPage();
 
-        }
-        else{
+        } else {
             new LoginFailedException("Admin Login Failed");
             return false;
         }
         return true;
     }
 
-    public void adminClientMainPage() {
-        System.out.println("Welcome to admin main page!!");
-    }
-
-    public void adminChangePassword(String userName,String old_password,String new_password){
-        if(Objects.equals(old_password, admin.getPassword())){
+    public void adminChangePassword(String userName, String old_password, String new_password) {
+        if (Objects.equals(old_password, admin.getPassword())) {
             admin.setPassword(new_password);
             System.out.println("Successfully changed the password");
-        }
-        else{
+        } else {
             System.out.println("Entered wrong password");
+        }
+    }
+
+    public void adminClientMainPage() {
+        LocalDateTime currentTime = LocalDateTime.now();
+        DateTimeFormatter myFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String formattedDate = currentTime.format(myFormat);
+        System.out.println("WELCOME ADMIN!!\nLogin Time: " + currentTime);
+        while (true) {
+            System.out.println("0. View All Gym Owners\n1. View Pending GymOwner Approval Requests\n" +
+                    "2. View Pending GymCenter's Approval Requests\n3. Go Back To Previous Menu");
+            int pendingChoice = scanner.nextInt();
+            switch (pendingChoice) {
+                case 0:
+                    System.out.println("All Gym Owners");
+                    break;
+                case 1:
+                    System.out.println("Pending GymOwner Approval Requests");
+                    break;
+                case 2:
+                    System.out.println("Pending GymCenter's Approval Requests");
+                    break;
+                case 3:
+                    return;
+            }
         }
     }
 }
