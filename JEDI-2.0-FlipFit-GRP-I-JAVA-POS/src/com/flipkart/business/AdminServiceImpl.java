f38aa4ea0f4f059b10f97cba9624872ab6cb0d18
Harshit Singh <harshitsinghwork@gmail.com>
2024-07-18 15:10:05 +0530

    table added for gym centre

--- JEDI-2.0-FlipFit-GRP-I-JAVA-POS/src/com/flipkart/business/AdminServiceImpl.java
+++ JEDI-2.0-FlipFit-GRP-I-JAVA-POS/src/com/flipkart/business/AdminServiceImpl.java
@@ -71,18 +71,20 @@ public class AdminServiceImpl implements AdminServiceInterface {
     public void viewAllGymCentres(){
         List<GymCentre> gymCentres = gymCenterDAO.getGymCentersList();
 
-        for(int i=0;i<gymCentres.size();i++){
-            System.out.println("---------------------------------------------------------------------------");
-            System.out.println("Gym Centre Id: " + gymCentres.get(i).getCentreId());
-            System.out.println("Gym Owner Id: " + gymCentres.get(i).getOwnerId());
-            System.out.println("Gym Centre Name: " + gymCentres.get(i).getCentreName());
-            System.out.println("GST No: " + gymCentres.get(i).getGstNo());
-            System.out.println("City: " + gymCentres.get(i).getCity());
-            System.out.println("Capacity: " + gymCentres.get(i).getCapacity());
-            System.out.println("Approved: " + gymCentres.get(i).isApproved());
-            System.out.println("Amount per slot: " + gymCentres.get(i).getAmountPerSlot());
-            System.out.println("---------------------------------------------------------------------------");
+//        List<String> gymCenterNames = new ArrayList<>();
+//        gymCentres.forEach(gymCentre -> {gymCenterNames.add(gymCentre.getCentreName());});
+        System.out.println("--------------------------------------------------------------------------------------------------------------------------------");
+        System.out.printf("| %-10s | %-10s | %-20s | %-15s | %-15s | %-8s | %-10s | %-15s |\n",
+                "Centre ID", "Owner ID", "Centre Name", "GST No", "City", "Capacity", "Approved", "Amount/Slot");
+
+        for (GymCentre gymCentre : gymCentres) {
+            System.out.println("--------------------------------------------------------------------------------------------------------------------------------");
+            System.out.printf("| %-10s | %-10s | %-20s | %-15s | %-15s | %-8d | %-10b | %-15d |\n",
+                    gymCentre.getCentreId(), gymCentre.getOwnerId(), gymCentre.getCentreName(), gymCentre.getGstNo(), gymCentre.getCity(), gymCentre.getCapacity(), gymCentre.isApproved(), gymCentre.getAmountPerSlot());
         }
+        System.out.println("--------------------------------------------------------------------------------------------------------------------------------");
+
+//        System.out.println(gymCenterNames);
 
     }
 }
