2df5b9812c844cd705cbf622a81da4a790c1f6da
srishtayy <srishtidhir652@gmail.com>
2024-07-18 12:29:26 +0530

    Gym owner dao added

---
+++ JEDI-2.0-FlipFit-GRP-I-JAVA-POS/src/com/flipkart/dao/GymOwnerInterfaceDao.java
@@ -0,0 +1,11 @@
+package com.flipkart.dao;
+import com.flipkart.bean.GymOwner;
+
+import java.util.List;
+
+public interface GymOwnerInterfaceDao {
+    public List<GymOwner> getGymOwnerList();
+    public void setGymOwnerList(List<GymOwner> gymOwnerList);
+    public void registerGymOwner(String userName,String password,String email,String panNumber,String cardNumber);
+    public List<GymOwner> getPendingGymOwnerList();
+}
