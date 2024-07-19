8b121ba9998df094af0e734d0f943656887f53ce
Himanshipahuja <hpahuja_be20@thapar.edu>
2024-07-19 12:19:00 +0530

    DB Connection added

---
+++ JEDI-2.0-FlipFit-GRP-I-JAVA-POS/src/com/flipkart/utils/DBConnection.java
@@ -0,0 +1,33 @@
+package com.flipkart.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+//    private static Connection singleInstance = null;
+    public static Connection connect() throws SQLException
+     {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/flipfit-schema";
+            Connection connection = DriverManager.getConnection(url,"root","password");
+                System.out.println("Database Connected");
+
+            return connection;
+//            PreparedStatement stmt=connection.prepareStatement("insert into role values(?,?,?)");
+//            stmt.setString(1,"sampleRoleId"); //1 specifies the first parameter in the query
+//            stmt.setString(2,"sampleRole");
+//            stmt.setString(3,"sampleRoleDesc");
+//
+//            int i=stmt.executeUpdate();
+//            System.out.println(i+" records inserted");
+
+        }
+        catch (Exception e) {
+            System.out.println("Could not connect to DB: " + e.getMessage() + " " + e);
+            return null;
+        }
+    }
+}
