376e169b80a99c1c0442ee22bd97e5f366a44c1f
Mahak5457 <mahakgarg1008@gmail.com>
2024-07-22 16:12:49 +0530

    Adding comments to booking service

--- JEDI-2.0-FlipFit-GRP-I-JAVA-POS/src/com/flipkart/business/BookingServiceImpl.java
+++ JEDI-2.0-FlipFit-GRP-I-JAVA-POS/src/com/flipkart/business/BookingServiceImpl.java
@@ -1,14 +1,16 @@
 package com.flipkart.business;
 
-import com.flipkart.bean.Booking;
 import com.flipkart.bean.BookingDetails;
 import com.flipkart.dao.BookingDAOImpl;
 
-import java.util.Date;
 import java.util.List;
 import com.flipkart.exceptions.BookingFailedException;
 
+/**
+ * Implementation of Booking Service.
+ */
 public class BookingServiceImpl implements BookingServiceInterface{
+
     private final BookingDAOImpl bookingDAO = new BookingDAOImpl();
 
     @Override
@@ -19,13 +21,9 @@ public class BookingServiceImpl implements BookingServiceInterface{
     @Override
     public void cancelBooking(String bookingID) {
         try {
-            Booking booking  = bookingDAO.getBookingByBookingId(bookingID);
             bookingDAO.cancelBookingById(bookingID);
-//            scheduleService.modifySchedule(booking.getScheduleID(),1);
         } catch (BookingFailedException e) {
             System.out.println(e.getMessage());
         }
-
     }
-
 }
--- JEDI-2.0-FlipFit-GRP-I-JAVA-POS/src/com/flipkart/business/BookingServiceInterface.java
+++ JEDI-2.0-FlipFit-GRP-I-JAVA-POS/src/com/flipkart/business/BookingServiceInterface.java
@@ -5,8 +5,24 @@ import com.flipkart.bean.BookingDetails;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * Interface for Booking Service.
+ */
 public interface BookingServiceInterface {
+
+    /**
+     * Retrieves a list of bookings for the specified customer.
+     *
+     * @param  username  the username of the customer
+     * @return a list of BookingDetails objects representing the customer's bookings
+     */
     List<BookingDetails> getBookingByCustomerId(String username);
+
+    /**
+     * Cancels a booking for the specified customer.
+     *
+     * @param  bookingID  the ID of the booking to be cancelled
+     */
     void cancelBooking(String bookingID);
 
 }
